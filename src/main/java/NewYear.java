b6fc3997af47e61bf6af8573b3862c3b81af3474
willu2 <bob@example.com>
2019-04-10 20:51:09 +0300

    vn1

--- src/main/java/NewYear.java
+++ src/main/java/NewYear.java
@@ -1,12 +1,7 @@
 public class NewYear {
     public static void main(String[] args) {
 
-        DataCreator creator = new DataCreator();
-       // creator.showaAllData();
-//        creator.printTypes();
-
         ShowMenu showMenu = new ShowMenu();
         showMenu.show();
-
     }
 }
